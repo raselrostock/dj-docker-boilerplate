c9ebd6d1930cd48e3b8f136009f6bf2476ececbb
raselrostock <raselrostock@gmail.com>
2020-04-30 11:08:53 +0600

    :art: Add: Added production level settings files

--- app/djdocker/settings/production.py
+++ app/djdocker/settings/production.py
@@ -3,6 +3,11 @@ from .base import *
 ################################
 ##     BASE CONFIGURATION     ##
 ################################
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = int(os.environ.get('DEBUG', default=0))
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = bool(os.environ.get("DEBUG", False))
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
@@ -11,13 +16,23 @@ from .base import *
 ##    DATABASE CONFIGURATION  ##
 ################################
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 ####################################
 ##  PASSWORD CONFIGURATION ##
 ####################################
